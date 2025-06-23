5ed579589020c5efb6e05671bceabda1a90bf1d8
rizzsv <rizq.syafriano@gmail.com>
2025-06-23 16:54:55 +0700

    feat: add observer to generate unique booking code (uniquecode)

---
+++ laravel/app/Observers/ProductSubscriptionObserver.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\ProductSubscription;
+
+class ProductSubscriptionObserver
+{
+
+
+    public function creating(ProductSubscription $subscription)
+    {
+        $subscription->booking_trx_id = $subscription->booking_trx_id ?? $this->generateUniqueTrxId();
+    }
+
+    private function generateUniqueTrxId(): string
+    {
+        $prefix = 'SPLITEASE';
+        do {
+             $randomString = $prefix . mt_rand(1000, 9999);
+        } while (ProductSubscription::where('booking_trx_id', $randomString)->exist()); 
+           
+        return $randomString;
+    }
+
+
+    /**
+     * Handle the ProductSubscription "created" event.
+     */
+    public function created(ProductSubscription $productSubscription): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ProductSubscription "updated" event.
+     */
+    public function updated(ProductSubscription $productSubscription): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ProductSubscription "deleted" event.
+     */
+    public function deleted(ProductSubscription $productSubscription): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ProductSubscription "restored" event.
+     */
+    public function restored(ProductSubscription $productSubscription): void
+    {
+        //
+    }
+
+    /**
+     * Handle the ProductSubscription "force deleted" event.
+     */
+    public function forceDeleted(ProductSubscription $productSubscription): void
+    {
+        //
+    }
+}
